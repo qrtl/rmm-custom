ed59a97bc7b9528c68e0aa5da5c44f1eee3ab2d5
Aungkokolin1997 <lin@quartile.co>
2025-03-19 09:44:37 +0000

    [ADD] product_attachment_name_update

---
+++ product_attachment_name_update/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Attachment Name Update",
+    "version": "15.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "installable": True,
+}
