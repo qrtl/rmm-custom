4e470714da9b41fd6b9635ed5285c28e8c2341c3
nobuQuartile <nobu@quartile.co>
2025-06-09 09:18:01 +0000

    delete chatgpt_integration

--- chatgpt_integration/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Initialize the models
-from . import ir_attachment
-from . import res_company
-from . import res_config_settings
-from . import openai_vision_message
-from . import openai_vision_session
--- chatgpt_integration/models/res_company.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    openai_api_key = fields.Char(string='OpenAI API Key')
